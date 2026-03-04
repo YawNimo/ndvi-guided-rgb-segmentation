2018e2d59b02b595f03e13afe18a219142884b81
Ellie Hurdis <245290573+elliehurdis@users.noreply.github.com>
2026-03-04 02:35:23 +0000

    update to new args and new env constants

--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
 import torch
 from pathlib import Path
 from build_models import build_model
-from utils import load_checkpoint, parse_args
+from utils import parse_args
+from io_utils import load_checkpoint
 from inference import inference_loop
-from io_helpers import plot_triplets
+from visualization_utils import plot_triplets
+from env_constants import INPUT_IMAGES_SUBDIR, INPUT_MASKS_SUBDIR, INPUT_PRED_MASKS_SUBDIR
 
 
 
@@ -13,25 +15,17 @@ if __name__ == "__main__":
     device = torch.device(args.device)
 
     tiles_base = Path(args.tiles_base)
-    img_dir = tiles_base / "images"
-    msk_dir = tiles_base / "masks"
+    img_dir = tiles_base / INPUT_IMAGES_SUBDIR
+    msk_dir = tiles_base / INPUT_MASKS_SUBDIR
 
     results_dir = Path(args.results_dir)
-    pred_dir = results_dir / args.model / "pred_masks"
+    pred_dir = results_dir / args.model / INPUT_PRED_MASKS_SUBDIR
     
     ckpt_path = Path(args.ckpt)
 
-    # Select tile list
-    if args.fixed:
-        s = args.fixed_tiles.strip()
-        if s.startswith("["):
-            tile_names = json.loads(s)
-        else:
-            tile_names = [x.strip() for x in s.split(",") if x.strip()]
-    else:
-        tile_names = sorted([p.name for p in img_dir.glob("*.png")])
-        if args.limit and args.limit > 0:
-            tile_names = tile_names[: args.limit]
+    tile_names = sorted([p.name for p in img_dir.glob("*.tif")])
+    if args.limit and args.limit > 0:
+        tile_names = tile_names[: args.limit]
 
     # Build + load model
     model = build_model(
@@ -52,8 +46,8 @@ if __name__ == "__main__":
         device=device
     )
 
-    # Visualization (only meaningful for fixed tiles)
-    if args.fixed:
+    # Visualization (only works up to a max of 3 tiles)
+    if args.make_viz and len(tile_names) <= 3:
         # TODO: move this to some visualization.py
         viz_fp = results_dir / args.model / "fixed_viz.png"
         plot_triplets(
